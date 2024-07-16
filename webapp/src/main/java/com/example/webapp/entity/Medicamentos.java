f21a8004cd0b3d5cdda9d4fa2df24333dd337e64
Julio Cesar <a20203521@pucp.edu.pe>
2024-07-15 23:25:11 -0500

    inventario mayor a 200 unidades y menor a 1000

--- webapp/src/main/java/com/example/webapp/entity/Medicamentos.java
+++ webapp/src/main/java/com/example/webapp/entity/Medicamentos.java
@@ -24,6 +24,8 @@ public class Medicamentos {
     private byte[] foto;
 
     @Positive(message = "El inventario debe ser positivo")
+    @Min(value = 200, message = "El inventario debe ser mayor o igual a 200 unidades")
+    @Max(value = 1000, message = "El inventario debe ser menor o igual a 1000 unidades")
     private int inventario;
 
     @Positive(message = "El precio por unidad debe ser positivo")
